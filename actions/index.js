9113ee8281e84bb65172d16c5e74ca365cd2fa82
CorinneKelly <corinnemariekelly@gmail.com>
2017-04-06 14:20:24 -0400

    Done.

--- actions/index.js
+++ actions/index.js
@@ -1,24 +1,72 @@
-'use strict';
+'use strict'
 
-function addColumn (ev) {
+
+
+function addColumn (event) {
+	event.preventDefault()
+	
+	let newTable = this.state.table.map((row) => [...row, ''])
+
+	this.setState({
+		table: newTable
+	})
 }
 
-function addRow (ev) {
+function addRow (event) {
+	event.preventDefault()
+
+	let numOfColumns = this.state.table[0].length
+
+	this.setState({
+		table: [...this.state.table, Array(numOfColumns).fill('')]
+	})
 }
 
-function changeCell (rowIndex, columnIndex, ev) {
+function changeCell (rowIndex, columnIndex, event) {
+	let newTable = this.state.table
+	newTable[rowIndex][columnIndex] = event.target.value
+
+	this.setState({
+		table: newTable
+	})
 }
+
 
 function focusCell (rowIndex, columnIndex) {
+	this.setState({
+		focused: [rowIndex, columnIndex]
+	})
 }
 
 function blurCell () {
+	this.setState({
+		focused: null	
+	})
 }
 
-function removeRow (ev) {
+function removeRow (event) {
+	event.preventDefault()
+	
+	if (this.state.table.length <= 1) return
+	let newTable = this.state.table
+	newTable.pop()
+
+	this.setState({
+		table: newTable
+	})
 }
 
-function removeColumn (ev) {
+function removeColumn (event) {
+	event.preventDefault()
+	if (this.state.table[0].length <= 1) return
+	let newTable = this.state.table.map((row) => 
+		row.slice(0, -1)
+		// -1 refers to the number of items to be deleted starting from the last item in array
+	)
+
+	this.setState({
+		table: newTable
+	})
 }
 
 module.exports = {
@@ -29,4 +77,4 @@ module.exports = {
   blurCell,
   removeRow,
   removeColumn
-};
+}
--- components/Spreadsheet.js
+++ components/Spreadsheet.js
@@ -1,38 +1,52 @@
-'use strict';
+'use strict'
 
-const React = require('react');
-const actions = require('../actions');
-const Table = require('./Table');
+const React = require('react')
+const actions = require('../actions')
+const Table = require('./Table')
 
 class Spreadsheet extends React.Component {
   constructor (props) {
-    super(props);
+    super(props)
     this.state = {
-    };
+      focused: null,
+      table: [['']]
+    }
+
+    this.handleAddColumn = actions.addColumn.bind(this)
+    this.handleAddRow = actions.addRow.bind(this)
+    this.handleRemoveColumn = actions.removeColumn.bind(this)
+    this.handleRemoveRow = actions.removeRow.bind(this)
+    this.handleChange = actions.changeCell.bind(this)
+    this.handleFocus = actions.focusCell.bind(this)
+    this.handleBlur = actions.blurCell.bind(this)
 
   }
+
+
+
   render () {
     return (
       <div className='spreadsheet'>
         <h1>My little spreadsheet</h1>
 
         <div className='spreadsheet__buttons'>
-          <button>Add Column</button>
+          <button onClick={this.handleAddColumn} >Add Column</button>
           {' '}
-          <button>Add Row</button>
+          <button onClick={this.handleAddRow} >Add Row</button>
           {' '}
-          <button>Remove Column</button>
+          <button onClick={this.handleRemoveColumn} >Remove Column</button>
           {' '}
-          <button>Remove Row</button>
+          <button onClick={this.handleRemoveRow} >Remove Row</button>
         </div>
 
-        <Table />
+        <Table table={this.state.table} onBlur={this.handleBlur} onChange={this.handleChange} onFocus={this.handleFocus} />
 
-        <div className='spreadsheet__focused'>
+        <div className='spreadsheet__focused' >
+          {(this.state.focused != null) ? (this.state.focused[0] + " - " + this.state.focused[1]) : null}
         </div>
       </div>
-    );
+    )
   }
 }
 
-module.exports = Spreadsheet;
+module.exports = Spreadsheet
