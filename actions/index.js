7dad8b5206383185ec84ac559aa793a6577a59ef
hwalborn <hwalborn@gmail.com>
2017-04-06 10:30:18 -0400

    balls deep

--- actions/index.js
+++ actions/index.js
@@ -1,24 +1,63 @@
 'use strict';
 
 function addColumn (ev) {
+  ev.preventDefault()
+  const addColToRow = this.state.table.map(row => [...row, ''])
+
+  this.setState({
+    table: addColToRow
+  })
 }
 
 function addRow (ev) {
+  ev.preventDefault()
+  let addedRow = [...this.state.table, ['','']]
+
+  this.setState({
+    table: addedRow
+  })
 }
 
 function changeCell (rowIndex, columnIndex, ev) {
+  let newTable = this.state.table
+  newTable[rowIndex][columnIndex] = ev.target.value
+  this.setState({
+    table: newTable
+  })
 }
 
 function focusCell (rowIndex, columnIndex) {
+  this.setState({
+    focused: [rowIndex, columnIndex]
+  })
 }
 
 function blurCell () {
+  this.setState({
+    focused: null
+  })
 }
 
 function removeRow (ev) {
+  ev.preventDefault()
+  if(this.state.table.length !== 1){
+    let removedRow = this.state.table.slice(0, -1)
+
+    this.setState({
+      table: removedRow
+    })
+  }
 }
 
 function removeColumn (ev) {
+  ev.preventDefault()
+  if(this.state.table[0].length !== 1){
+    let newTable = this.state.table.map(row => row.slice(0, -1))
+
+    this.setState({
+      table: newTable
+    })
+  }
 }
 
 module.exports = {
--- components/Row.js
+++ components/Row.js
@@ -5,6 +5,15 @@ const Cell = require('./Cell');
 
 class Row extends React.Component {
   render () {
+    let cells = []
+    for(var i = 0; i < this.props.row.length; i++){
+      cells.push(<Cell key={i} cell={this.props.row[i]} onChange={this.props.onChange.bind(null, i)} onFocus={this.props.onFocus.bind(null, i)} />)
+    }
+    return (
+      <tr className="row">
+        {cells}
+      </tr>
+    )
   }
 }
 
