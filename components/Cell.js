d2ecaf3d2088d1043908779f092c6698ff4327b1
gee2789 <geeyung.li@gmail.com>
2017-04-08 13:07:18 -0400

    Done.

--- components/Cell.js
+++ components/Cell.js
@@ -3,6 +3,16 @@
 import React from 'react';
 
 export default class Cell extends React.Component {
+
   render () {
+    return(
+    <td className='cell'>
+      <input  value={this.props.cell}
+              onChange={this.props.onChange}
+              onFocus={this.props.onFocus}
+              >
+      </input>
+    </td>
+    )
   }
 }
--- components/Table.js
+++ components/Table.js
@@ -4,6 +4,24 @@ import React from 'react';
 import Row from './Row';
 
 export default class Table extends React.Component {
+
+
   render () {
+    return(
+      <table className="table" onBlur={this.props.onBlur}>
+        <tbody>
+          {
+            this.props.table.map((row, rowIndex)=>
+            <Row
+              key={rowIndex}
+              row={row}
+              onChange={this.props.onChange.bind(null, rowIndex)}
+              onFocus={this.props.onFocus.bind(null, rowIndex)}
+              />
+            )
+          }
+        </tbody>
+      </table>
+    )
   }
 }
