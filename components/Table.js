def0f29385c1230d441b44117a0a20fb3da78ced
vrathsmill <vlrathsmill@gmail.com>
2017-04-09 16:22:43 -0400

    Done.

--- components/Table.js
+++ components/Table.js
@@ -1,11 +1,24 @@
 'use strict';
-
 const React = require('react');
 const Row = require('./Row');
-
 class Table extends React.Component {
   render () {
+    return(
+      <table className="table" onBlur={this.props.onBlur}>
+        <tbody>
+          {
+            this.props.table.map((row, rowIndex) =>
+              <Row
+                key={rowIndex}
+                row={row}
+                onChange={this.props.onChange.bind(null, rowIndex)}
+                onFocus={this.props.onFocus.bind(null, rowIndex)}
+              />
+            )
+          }
+        </tbody>
+      </table>
+    )
   }
 }
-
 module.exports = Table;
