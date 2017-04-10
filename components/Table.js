c50efcb59fca89198563f533159d2abb8f51bedf
github username <github email address>
2017-04-10 13:22:11 -0400

    Done.

--- components/Table.js
+++ components/Table.js
@@ -4,6 +4,25 @@ import React from 'react';
 import Row from './Row';
 
 export default class Table extends React.Component {
+  // constructor (props) {
+  //   super(props)
+  // }
+
   render () {
+    const rows = this.props.table.map((row, index) => {
+      return <Row
+              key={index}
+              row={row}
+              onChange={this.props.onChange.bind(null, index)}
+              onFocus={this.props.onFocus.bind(null, index)}
+                />
+    })
+    return (
+      <table className='table' onBlur={this.props.onBlur}>
+        <tbody>
+          {rows}
+          </tbody>
+        </table>
+    )
   }
 }
