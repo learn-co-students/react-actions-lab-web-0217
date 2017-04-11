33281df750444077c99314b26dae9f1fd010e45d
fattyduck <dison.ruan@yahoo.com>
2017-04-11 09:11:05 -0400

    Done.

--- actions/index.js
+++ actions/index.js
@@ -1,24 +1,49 @@
 'use strict';
 
 function addColumn (ev) {
+  const table = this.state.table.map((row) => {
+    return row.concat("")
+  })
+  this.setState({table})
+  ev.preventDefault()
 }
 
 function addRow (ev) {
+  const row = this.state.table[0].reduce((arr) => arr.concat(""),[])
+  this.state.table.push(row)
+  this.setState({table: this.state.table})
+  ev.preventDefault()
 }
 
 function changeCell (rowIndex, columnIndex, ev) {
+  const table = this.state.table
+  table[rowIndex][columnIndex] = ev.target.value
+  this.setState({table})
 }
 
 function focusCell (rowIndex, columnIndex) {
+  let focused = [rowIndex, columnIndex]
+  this.setState({focused})
 }
 
 function blurCell () {
+  this.setState({focused: null})
 }
 
 function removeRow (ev) {
+  let table = this.state.table
+  if(table.length > 1) {
+    table.pop()
+    this.setState({table})
+  }
+  ev.preventDefault();
 }
 
 function removeColumn (ev) {
+  ev.preventDefault();
+  if (this.state.table[0].length < 2) return
+  let table = this.state.table.map((row) => row.slice(0,-1))
+  this.setState({table})
 }
 
 module.exports = {
--- components/Row.js
+++ components/Row.js
@@ -5,5 +5,16 @@ import Cell from './Cell';
 
 export default class Row extends React.Component {
   render () {
+    const {row, onChange, onFocus} = this.props
+    return(
+      <tr className="row">
+        {row.map((cell, index) => {
+          return <Cell cell={cell}
+            onChange={this.props.onChange.bind(null, index)}
+            onFocus={this.props.onFocus.bind(null, index)}
+            key={index}/>
+        })}
+      </tr>
+    )
   }
 }
