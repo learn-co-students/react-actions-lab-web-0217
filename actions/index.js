dc6be2a7cdbde28125bca64e427828edebf91b5c
NickAnaya618 <anaya.nicholas@gmail.com>
2017-04-10 21:30:58 -0400

    Done.

--- actions/index.js
+++ actions/index.js
@@ -1,24 +1,81 @@
 'use strict';
 
 function addColumn (ev) {
+  ev.preventDefault()
+  let newTable = this.state.table.map((row) => {
+    row.push('')
+    return row
+  })
+  this.setState({
+    table: newTable
+  });
 }
 
 function addRow (ev) {
+  ev.preventDefault()
+  let newRow = []
+  let rowLength = this.state.table[0].length
+
+  for(var i=0; i < rowLength; i++){
+    newRow.push('')
+  }
+
+  let tempTable = this.state.table
+
+  tempTable.push(newRow)
+
+  this.setState({
+    table: tempTable
+  })
 }
 
 function changeCell (rowIndex, columnIndex, ev) {
+  let tempTable = this.state.table
+  tempTable[rowIndex][columnIndex] = ev.target.value
+
+  this.setState({
+    table: tempTable
+  })
 }
 
 function focusCell (rowIndex, columnIndex) {
+  let focusedCell = [rowIndex, columnIndex]
+
+  this.setState({
+    focused: focusedCell
+  })
 }
 
 function blurCell () {
+  this.setState({
+    focused: null
+  })
 }
 
 function removeRow (ev) {
+  ev.preventDefault()
+  let tempTable = this.state.table
+  if (tempTable.length > 1){
+    tempTable.pop()
+
+    this.setState({
+      table: tempTable
+    })
+  }
 }
 
 function removeColumn (ev) {
+  ev.preventDefault()
+  let tempTable = this.state.table
+  if (tempTable[0].length > 1){
+    tempTable.map((row) => {
+      row.pop()
+    })
+
+    this.setState({
+      table: tempTable
+    })
+  }
 }
 
 module.exports = {
--- components/Table.js
+++ components/Table.js
@@ -5,5 +5,19 @@ import Row from './Row';
 
 export default class Table extends React.Component {
   render () {
+    return (
+      <table className='table' onBlur={this.props.onBlur}>
+        <tbody>
+          {this.props.table.map((element, index) =>
+            <Row
+              key={index}
+              row={element}
+              onChange={this.props.onChange.bind(null, index)}
+              onFocus={this.props.onFocus.bind(null, index)}
+            />
+          )}
+        </tbody>
+      </table>
+    )
   }
 }
